6e41db0832acd2954da62eaa7b623a8d1d13a340
Shahzeb Jadoon <shahzebjadoon0@gmaile.com>
2023-10-14 01:54:38 +0500

    fix: :fire: fixed form

--- src/components/tabs/Taxonomy.jsx
+++ src/components/tabs/Taxonomy.jsx
@@ -11,21 +11,22 @@ const Taxonomy = () => {
   return (
     <div className="flex flex-col h-full">
       <div className="flex-1">
-        <div className="p-5 flex-between flex-wrap  gap-5 w-full">
-          <div className="flex-between flex-wrap gap-1 w-full ">
-            <div className="w-full md:w-[48%]">
+        <div className="p-5 flex-between flex-wrap gap-5 w-full">
+          <div className="flex-between flex-wrap gap-2 w-full ">
+            <div className="w-full md:w-[49%]">
               <Input label="Project Name" />
             </div>
-            <div className="w-full  md:w-[48%]">
+            <div className=" w-full md:w-[49%]">
               <DropDown options={drpItems} label="Options" />
+              {/* <p className="body-regular mt-2">Add another option</p> */}
             </div>
           </div>
 
           <div className="flex-between flex-wrap w-full">
-            <div className="w-full   md:w-[48%]">
+            <div className="w-full   md:w-[49%]">
               <Input label="Question" />
             </div>
-            <div className="w-full  md:w-[48%]">
+            <div className="w-full  md:w-[49%]">
               <DropDown
                 options={drpItems}
                 label="Reference Class"
@@ -40,31 +41,31 @@ const Taxonomy = () => {
           </div>
 
           <div className="flex-between  flex-wrap  justify-between w-full">
-            <div className="w-full md:w-[48%] ">
+            <div className="w-full md:w-[49%] ">
               <Input label="Label" />
             </div>
-            <div className="flex-between w-full  md:w-[48%] gap-2">
-              <div className="w-full md:w-[48%] ">
+            <div className="flex-between w-full  md:w-[49%] gap-2">
+              <div className="w-full md:w-[49%] ">
                 <NumberInput label="Evaluation Page Layout" />
               </div>
-              <div className=" md:w-[48%] w-full ">
+              <div className=" md:w-[49%] w-full ">
                 <NumberInput />
               </div>
             </div>
           </div>
 
-          <div className="flex-between flex-wrap w-full ">
-            <div className="flex-between w-full gap-2">
-              <div className="w-full  md:w-[48%]">
+          <div className="flex flex-wrap w-full ">
+            <div className="flex gap-1 w-1/2 ">
+              <div className="w-full md:w-[49%]">
                 <NumberInput label="Labels" />
               </div>
-              <div className="w-full  md:w-[48%]">
+              <div className="w-full md:w-[49%]">
                 <NumberInput />
               </div>
             </div>
           </div>
         </div>
-        <div className="p-5 w-1/2">
+        <div className="pl-5 md:w-[49%]">
           <DropDown label="Select Option" options={drpItems} />
         </div>
       </div>
